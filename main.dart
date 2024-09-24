ce925d1ad72479c1107af17977dd9aa1ac5b0e6b
MuhammadUsmanAfridii <70139706@student.uol.edu.pk>
2024-09-24 10:48:25 +0500

    First code

--- main.dart
+++ main.dart
@@ -1,3 +1,14 @@
 void main() {
-  print('Hello, World!');
+  print('First Lab Dart Installation');
+  print('Lab Practice');
+
+//   var variable = 10;
+// print(variable);
+
+// dynamic variable = 10;
+// print(variable + 25);
+
+String variable = "Usman";
+print(variable);
+
 }
