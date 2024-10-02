d7640425f384fc03270a0f692273bd3718dc8f5b
MuhammadUsmanAfridii <161357183+MuhammadUsmanAfridii@users.noreply.github.com>
2024-10-02 10:14:05 +0500

    Add files via upload

---
+++ lab3-prac.dart
@@ -0,0 +1,63 @@
+// void main (){
+//   Set<int>num={};
+//   print(num):
+// }
+
+// void main(){
+//   Set<int>num={10,20,30};
+//   print(num);
+// }
+
+// void main(){
+//   Set<int>num={10,20,30};
+//   print(num.remove(20));
+//   print(num);
+// }
+
+// void main(){
+//   Set<int>num={10,20,30};
+//   print(num.contains(215));
+// }
+
+// void main(){
+//   Set<int>otherset={10,20,30};
+//   print(otherset);
+// }
+
+// void main(){
+//    Set<int>myset={10,20,30};
+//    Set<int>otherset={10,20,30};
+//    Set<int>unionset=myset.union(otherset);
+//    print(unionset);
+// }
+
+// void main(){
+//    Set<int>myset={10,20,30};
+//    Set<int>otherset={10,20,30};
+//    Set<int>unionset=myset.intersection(otherset);
+//    print(unionset);
+// }
+
+// void main(){
+//    Set<int>myset={10,20,30};
+//    Set<int>otherset={10,20,30};
+//    Set<int>unionset=myset.difference(otherset);
+//    print(unionset);
+// }
+
+// void main(){
+//   Set<int>myset={10,20,30};
+//   print(myset);myset.foreach((element)=>10,);
+// }
+
+// void main(){
+//   Set<String>employeeSet={'usman','khan'};
+//   List<String>employee_tst=employeeSet.toList();
+//   var employeemap = employeeSet.map((value){
+//     return '$value';
+//  });
+// print(employee_tst);
+// print(employeeSet);
+// print(employeemap);
+
+// }
